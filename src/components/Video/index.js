125c7b0b94efce17630122f70212550aaaea85cc
Ensi Junior Entreprise <68879202+ensi-junior-entreprise@users.noreply.github.com>
2020-11-27 22:51:29 +0100

    Update index.js

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import useIsInViewport from 'use-is-in-viewport'
 import Animation from '../Animation'
 import {Reveal} from 'react-reveal';
+import { ExternalLink } from 'react-external-link';
 export default function Video (props) {   
             
 
@@ -16,10 +17,11 @@ export default function Video (props) {
                         <div className='video-conainer' >
                             <h3 className='test' >Always Striving For Greatness</h3>
                             <h1>La 15ème édition du forum annuel de l'ENSI</h1>
-                            <Countdown
+                            <ExternalLink href="http://eventzoo-forumensi-11-2020.s3.eu-west-3.amazonaws.com/index.html" target={'_blank'}><h1 style={{fontSize:"50px"}}>Rejoindre la plateforme</h1></ExternalLink>
+                    {/*<Countdown
                             date={new Date("November 28, 2020 8:30:00")}
                             renderer={Renderer}
-                            />
+                            />*/}
                             <Link to="/register" className="btn-outline" style={{marginTop:"3%"}}>S'inscrire</Link>
                         </div> 
                     </Reveal>  

55e6d1f1df12c2e1b54b8a74cebbf285ef36959b
Ensi Junior Entreprise <68879202+ensi-junior-entreprise@users.noreply.github.com>
2020-11-27 21:16:06 +0100

    Update LogoItem.js

--- src/components/BrandLogo/LogoItem.js
+++ src/components/BrandLogo/LogoItem.js
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 function LogoItem(props) {
     return (
         <div className="brand-logo-item">
-            <Link to={`${process.env.PUBLIC_URL + props.URL}`}>
+            
                 <img src={require('../../assets/img/' + props.logoSrc)} alt="Businex-Logo"/>
-            </Link>
+            
         </div>
     );
 }
